f36807afaabc06b2152b8da8957f283c5e295823
Amir Motefaker <amotef@gmail.com>
2022-06-19 12:13:06 +0430

    Create password_authentication.py

---
+++ password_authentication.py
@@ -0,0 +1,14 @@
+# Code by @AmirMotefaker
+
+# Password Authentication
+
+import getpass
+database = {"amir": "123456", "motefaker": "654321"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
