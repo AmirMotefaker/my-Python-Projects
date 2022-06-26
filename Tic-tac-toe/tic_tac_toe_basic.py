23b2e9486562bb5866a0e157828be9ed13dcb977
Amir Motefaker <amotef@gmail.com>
2022-06-26 14:02:35 +0430

    Update tic_tac_toe_basic.py

--- Tic-tac-toe/tic_tac_toe_basic.py
+++ Tic-tac-toe/tic_tac_toe_basic.py
@@ -3,23 +3,6 @@
 # Tic-Tac-Toe Game (Basic)
 
 
-# Step-by-Step
-# 1- Create a board using a 2-dimensional array and initialize each element as empty.
-# 2- Write a function to check whether the board is filled or not.
-# 3- Write a function to check whether a player has won or not.
-# 4- Write a function to show the board as we will show the board multiple times to the users while they are playing.
-# 5- Write a function to start the game:
-#   Select the first turn of the player randomly.
-#   Write an infinite loop that breaks when the game is over (either win or draw).
-#   Show the board to the user to select the spot for the next move.
-#   Ask the user to enter the row and column number.
-#   Update the spot with the respective player sign.
-#   Check whether the current player won the game or not.
-#   If the current player won the game, then print a winning message and break the infinite loop.
-#   Next, check whether the board is filled or not.
-#   If the board is filled, then print the draw message and break the infinite loop.
-# Finally, show the user the final view of the board.
-
 import random
 
 
