c725e3848517c2d45f4efa0e5b29a8aab54c1885
Amir Motefaker <amotef@gmail.com>
2022-07-01 08:43:41 +0430

    Update basic_calculator.py
    Added output and remove comment

--- Calculator/basic_calculator.py
+++ Calculator/basic_calculator.py
@@ -3,19 +3,15 @@
 # Basic Calculator
 
 
-# This function adds two numbers
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
     return x / y
 
@@ -27,10 +23,8 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
-    # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
 
-    # check if choice is one of the four options
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
@@ -47,12 +41,37 @@ while True:
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
         
-        # check if user wants another calculation
-        # break the while loop if answer is no
+        
         next_calculation = input("Let's do next calculation? (yes/no): ")
         if next_calculation == "no":
           break
     
     else:
         print("Invalid Input")
-        
+
+
+# Output:
+# Select operation.
+# 1.Add
+# 2.Subtract
+# 3.Multiply
+# 4.Divide
+# Enter choice(1/2/3/4): 1
+# Enter first number: 1
+# Enter second number: 2
+# 1.0 + 2.0 = 3.0
+# Let's do next calculation? (yes/no): yes
+# Enter choice(1/2/3/4): 2
+# Enter first number: 2
+# Enter second number: 1
+# 2.0 - 1.0 = 1.0
+# Let's do next calculation? (yes/no): n
+# Enter choice(1/2/3/4): 0
+# Invalid Input
+# Enter choice(1/2/3/4): 65
+# Invalid Input
+# Enter choice(1/2/3/4): 4
+# Enter first number: 2
+# Enter second number: 1
+# 2.0 / 1.0 = 2.0
+# Let's do next calculation? (yes/no): no   
