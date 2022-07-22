39e1c1c780ca5c7fa64635114fcbb46d4d1a387a
Amir Motefaker <amotef@gmail.com>
2022-07-22 10:42:26 +0430

    Update temperature_conversion.py
    Added padding  for label and button

--- Temperature Conversion/temperature_conversion.py
+++ Temperature Conversion/temperature_conversion.py
@@ -12,15 +12,16 @@ lbl_fahrenheit = tk.Label(
 )
 ent_fahrenheit = tk.Entry(
     master=window,
+    width=50,
 )
 btn_calc = tk.Button(
     master=window,
     text='Calc',
 )
 
-lbl_fahrenheit.grid(row=0, column=0)
+lbl_fahrenheit.grid(row=0, column=0, padx=10, pady=10)
 ent_fahrenheit.grid(row=0, column=1)
-btn_calc.grid(row=0, column=2)
+btn_calc.grid(row=0, column=2, padx=10, pady=10)
 
 lbl_celsius = tk.Label(
     master=window,
@@ -31,7 +32,7 @@ lbl_result = tk.Label(
     text='Enter your number...'
 )
 
-lbl_celsius.grid(row=1, column=0)
-lbl_result.grid(row=1, column=1)
+lbl_celsius.grid(row=1, column=0, pady=(10, 20))
+lbl_result.grid(row=1, column=1, pady=(10, 20))
 
 window.mainloop()
