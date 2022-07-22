69f3832b7514ba94170a6d8a799cf467a15d4445
Amir Motefaker <amotef@gmail.com>
2022-07-22 10:32:37 +0430

    Update temperature_conversion.py
    Added label celsius

--- Temperature Conversion/temperature_conversion.py
+++ Temperature Conversion/temperature_conversion.py
@@ -22,4 +22,16 @@ lbl_fahrenheit.grid(row=0, column=0)
 ent_fahrenheit.grid(row=0, column=1)
 btn_calc.grid(row=0, column=2)
 
+lbl_celsius = tk.Label(
+    master=window,
+    text='Celsius:',
+)
+lbl_result = tk.Label(
+    master=window,
+    text='Enter your number...'
+)
+
+lbl_celsius.grid(row=1, column=0)
+lbl_result.grid(row=1, column=1)
+
 window.mainloop()
