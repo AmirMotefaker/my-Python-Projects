3a1c173737f0846edd7a101997d683f972abb1d2
Amir Motefaker <amotef@gmail.com>
2022-07-23 12:51:09 +0430

    Create GUI_calculator.py

---
+++ Calculator/GUI_calculator.py
@@ -0,0 +1,17 @@
+# Code by @AmirMotefaker
+
+# GUI - Calculator
+
+import tkinter as tk
+
+window = tk.Tk()
+
+lbl_calc_result = tk.Label(
+    master=window,
+    text='0',
+    width=30,
+    height=3,
+)
+lbl_calc_result.grid(row=0, column=0)
+
+window.mainloop()
