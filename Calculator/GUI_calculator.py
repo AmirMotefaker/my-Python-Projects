9daf84dc36cc641b13fc5257327f5a6ceedeea3b
Amir Motefaker <amotef@gmail.com>
2022-07-23 13:01:21 +0430

    Update GUI_calculator.py
    Added Button 7,8,9,+

--- Calculator/GUI_calculator.py
+++ Calculator/GUI_calculator.py
@@ -12,6 +12,35 @@ lbl_calc_result = tk.Label(
     width=30,
     height=3,
 )
-lbl_calc_result.grid(row=0, column=0)
+lbl_calc_result.grid(row=0, column=0, columnspan=4)
+
+btn_7 = tk.Button(
+    master=window,
+    text='7',
+    command=lambda: print('7'),
+)
+btn_7.grid(row=1, column=0)
+
+btn_8 = tk.Button(
+    master=window,
+    text='8',
+    command=lambda: print('8'),
+)
+btn_8.grid(row=1, column=1)
+
+btn_9 = tk.Button(
+    master=window,
+    text='9',
+    command=lambda: print('9'),
+)
+btn_9.grid(row=1, column=2)
+
+btn_plus = tk.Button(
+    master=window,
+    text='+',
+    command=lambda: print('+'),
+)
+btn_plus.grid(row=1, column=3)
 
 window.mainloop()
+
