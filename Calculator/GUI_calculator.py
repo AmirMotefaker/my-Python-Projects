7a72340b23105409718345e5cd4fb37187747068
Amir Motefaker <amotef@gmail.com>
2022-07-24 14:31:54 +0430

    Update GUI_calculator.py
    Added def is_last_number_decimal(current)

--- Calculator/GUI_calculator.py
+++ Calculator/GUI_calculator.py
@@ -81,30 +81,40 @@ lbl_calc_result = tk.Label(
 )
 lbl_calc_result.grid(row=0, column=0, columnspan=4)
 
-last_op_index = -1
-last_dot_index = -1
+def is_last_number_decimal(current):
+    for char in current[::-1]:
+        if char == '.':
+            return True  
+        if char in ['+', '-', '*']:
+            return False
+
+    return False
+    
+# last_op_index = -1
+# last_dot_index = -1
 
 def insert_number_in_clac_result(btn_text):
     current = lbl_calc_result['text']
 
-    global last_op_index, last_dot_index
-    if btn_text in ['+', '-', '*']:
-        last_op_index = len(current)
+    # global last_op_index, last_dot_index
+    # if btn_text in ['+', '-', '*']:
+        # last_op_index = len(current)
     
     if btn_text == 'C':
         lbl_calc_result['text'] = '0'
-        last_op_index, last_dot_index = 0, 0
+        # last_op_index, last_dot_index = 0, 0
     elif current == '0':
         lbl_calc_result['text'] = btn_text
     elif btn_text == '=':
         result = f"{eval(current)}"
         lbl_calc_result['text'] = result
-        last_op_index, last_dot_index = 0, 0
-        if '.' in result:
-            last_dot_index = result.index('.')
-    elif btn_text == '.' and not (last_dot_index > last_op_index or current[-1] == '.'):
-        lbl_calc_result['text'] += btn_text
-        last_dot_index = len(current)
+        # last_op_index, last_dot_index = 0, 0
+        # if '.' in result:
+            # last_dot_index = result.index('.')
+    elif btn_text == '.': # and not (last_dot_index > last_op_index or current[-1] == '.'):
+        if not is_last_number_decimal(current):
+            lbl_calc_result['text'] += btn_text
+        # last_dot_index = len(current)
     elif btn_text in ['+', '-', '*'] and current[-1] in ['+', '-', '*']:
         lbl_calc_result['text'] = current[:-1] + btn_text
     else:
